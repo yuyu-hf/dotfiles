b179716459f1960d8944d60abe800b8087720bdb
yuyu-hf <yuyuhf.ds@gmail.com>
2026-01-22 00:18:43 +0900

    MacOS用のnixの設定をお試しで入れました

--- config/.config/zsh/common/init.zsh
+++ config/.config/zsh/common/init.zsh
@@ -1,3 +1,4 @@
+source "${HOME}/.config/zsh/common/nix.zsh"
 source "${HOME}/.config/zsh/common/aliases.zsh"
 source "${HOME}/.config/zsh/common/mise.zsh"
 source "${HOME}/.config/zsh/common/starship.zsh"
